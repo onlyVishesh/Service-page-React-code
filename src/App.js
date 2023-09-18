0e0907d8d8e6959d9db9b22f15c6a52624da4ec7
vibhav13122003 <vibhavbhartiya13@gmail.com>
2023-09-18 23:14:43 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Service from "./components/Service";
-
 function App() {
     return (
         <div className="App">
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,10 +1,18 @@
 import Quotes from "./Quotes";
-
-
+import Section1 from "./Section1";
+import AmbulanceTypes from "./AmbulanceTypes";
+import Section3 from "./Section3";
+import Section5 from "./Section5";
 const Service = () => {
     return ( 
     <main className="main">
+         <Section1/>
+            <AmbulanceTypes />
+
+         <Section3/>
+
         <Quotes />
+<Section5/>
 
     </main>
     );
